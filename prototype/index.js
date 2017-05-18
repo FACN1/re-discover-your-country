86c24131433e252d535a3fd93b8f0d43af3a2c44
Matt Lubel <matt_lubel@hotmail.com>
2017-05-18 17:38:06 +0300

    try to fix homepage buttons

--- prototype/index.js
+++ prototype/index.js
@@ -1,4 +1,4 @@
-var initialDelay = 1.5;
+var initialDelay = 1;
 TweenLite.from('.HomeBackGround', 1, {
   css: {
     'filter': 'blur(15px)','-webkit-filter': 'blur(15px)'
@@ -6,6 +6,6 @@ TweenLite.from('.HomeBackGround', 1, {
   ease:Power2.easeInOut,
   delay: initialDelay
 })
-TweenLite.from('#logo', 0.7, {bottom: -150, ease:Power2.easeInOut, delay: initialDelay})
+TweenLite.from('#logo', 0.7, {top: '50%', ease:Power2.easeInOut, delay: initialDelay})
 TweenLite.from('#homepage-text', 0.7, {opacity: 0, ease:Power2.easeInOut, delay: initialDelay + 0.5})
-TweenLite.from('#buttons-container', 0.7, {bottom: -300, ease:Power2.easeInOut, delay: initialDelay + 0.5})
+TweenLite.from('#buttons-container', 0.7, {top: '120%', ease:Power2.easeInOut, delay: initialDelay + 0.5})
